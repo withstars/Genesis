c143cf7937b5ba873fd4385533249b783ceaf13c
withstars <15535822816@163.com>
2018-01-18 20:45:41 +0800

    [update]controller

--- src/main/java/com/withstars/controller/MainController.java
+++ src/main/java/com/withstars/controller/MainController.java
@@ -4,12 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.jws.WebParam;
+
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
-    public ModelAndView toIndex(){
-        ModelAndView toIndex=new ModelAndView("login");
-        return  toIndex;
+
+
+    @RequestMapping(value = {"/signin"})
+    public ModelAndView signin(){
+        ModelAndView signinPage=new ModelAndView("signin");
+        return  signinPage;
+    }
+
+    @RequestMapping("/signup")
+    public ModelAndView signup(){
+        ModelAndView SignupPage=new ModelAndView("signup");
+        return  SignupPage;
     }
 }
