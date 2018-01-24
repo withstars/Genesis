6b5dd11eaff6340139410dc2d2420f13c6735dd4
withstars <15535822816@163.com>
2018-01-24 21:21:40 +0800

    [update]UserService

--- src/main/java/com/withstars/service/UserService.java
+++ src/main/java/com/withstars/service/UserService.java
@@ -4,7 +4,21 @@ import com.withstars.domain.User;
 
 public interface UserService {
 
+    //用户注册
     public boolean addUser(User user);
 
-    public boolean login(String username,String password);
+    //登录验证
+    public int login(String username,String password);
+
+    //添加积分
+    public boolean addCredit(Integer points,Integer id);
+
+    //username是否存在
+    public boolean existUsername(String username);
+
+    //登录后获取用户信息
+    public User getUserByUsername(String username);
+
+    //获取用户数
+    public int getUserCount();
 }
