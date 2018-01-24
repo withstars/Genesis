862d59559ce311a53f9ec8448290707119cdc126
withstars <15535822816@163.com>
2018-01-24 21:12:22 +0800

    [update]MainController

--- src/main/java/com/withstars/controller/MainController.java
+++ src/main/java/com/withstars/controller/MainController.java
@@ -3,6 +3,8 @@ package com.withstars.controller;
 import com.withstars.domain.Tab;
 import com.withstars.service.impl.ReplyServiceImpl;
 import com.withstars.service.impl.TabServiceImpl;
+import com.withstars.service.impl.TopicServiceImpl;
+import com.withstars.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +16,41 @@ import java.util.List;
 @Controller
 public class MainController {
 
-
     @Autowired
-    public TabServiceImpl tabService;
+    public TopicServiceImpl topicService;
     @Autowired
     public ReplyServiceImpl replyService;
+    @Autowired
+    public UserServiceImpl userService;
+    @Autowired
+    public TabServiceImpl tabService;
+
 
     //进入登录页面
     @RequestMapping(value = {"/signin"})
     public ModelAndView signin(){
         ModelAndView signinPage=new ModelAndView("signin");
+
+        //获取统计信息
+        int topicsNum=topicService.getTopicsNum();
+        int usersNum=userService.getUserCount();
+
+        signinPage.addObject("topicsNum",topicsNum);
+        signinPage.addObject("usersNum",usersNum);
         return  signinPage;
     }
     //进入注册页面
     @RequestMapping("/signup")
     public ModelAndView signup(){
-        ModelAndView SignupPage=new ModelAndView("signup");
-        return  SignupPage;
+        ModelAndView signupPage=new ModelAndView("signup");
+
+        //获取统计信息
+        int topicsNum=topicService.getTopicsNum();
+        int usersNum=userService.getUserCount();
+
+        signupPage.addObject("topicsNum",topicsNum);
+        signupPage.addObject("usersNum",usersNum);
+        return  signupPage;
     }
 
     //进入新建主题页面
@@ -38,7 +58,14 @@ public class MainController {
     public ModelAndView newTopic(){
         ModelAndView newTopicPage=new ModelAndView("new");
         List<Tab> tabs=tabService.getAllTabs();
+
+        //获取统计信息
+        int topicsNum=topicService.getTopicsNum();
+        int usersNum=userService.getUserCount();
+
         newTopicPage.addObject("tabs",tabs);
+        newTopicPage.addObject("topicsNum",topicsNum);
+        newTopicPage.addObject("usersNum",usersNum);
         return  newTopicPage;
     }
 }
