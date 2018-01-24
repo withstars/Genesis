6711eec9e921670c274fd63cd985fa41e38a71ed
withstars <15535822816@163.com>
2018-01-24 21:20:08 +0800

    [update]UserMapper

--- src/main/java/com/withstars/dao/UserMapper.java
+++ src/main/java/com/withstars/dao/UserMapper.java
@@ -1,19 +1,31 @@
 package com.withstars.dao;
 
 import com.withstars.domain.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
 
+    //用户注册
     int addUser(User user);
 
     int insertSelective(User record);
 
     User selectByPrimaryKey(Integer id);
 
+
     User selectByUsername(String username);
 
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    //多参数注解
+    int addCredit(@Param("points") Integer points,@Param("id")Integer id);
+
+    //查询username是否存在
+    int existUsername(String username);
+
+    //查询用户数
+    int getUserCount();
 }
