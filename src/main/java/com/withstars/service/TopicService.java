dd616a49bf7268cb7bbcc9fe75cb7abb1c9f0f73
withstars <15535822816@163.com>
2018-01-24 21:17:27 +0800

    [update]TopicService

--- src/main/java/com/withstars/service/TopicService.java
+++ src/main/java/com/withstars/service/TopicService.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 public interface TopicService {
 
-    //返回全部主题
+    //获取全部主题
     public List<Topic> getAllTopics();
-    //返回指定id主题
+    //获取指定ID主题
     public Topic selectById(Integer id);
     //新建主题
     public boolean addTopic(Topic topic);
     //点击量加一
     public boolean clickAddOne(Integer id);
+    //获取主题总数
+    public int getTopicsNum();
 }
