4402ad3d1c3678d27dd6ced3a0dac75397e6e14f
withstars <15535822816@163.com>
2018-02-02 23:02:25 +0800

    登录验证改为ajax方式

--- src/main/java/com/withstars/controller/UserController.java
+++ src/main/java/com/withstars/controller/UserController.java
@@ -9,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.MessageDigest;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 /**
@@ -99,17 +101,20 @@ public class UserController {
 
     /**
      * 用户登陆
+     * 返回值: 0:用户名不存在 1:密码错误 2:登录成功
      */
-    @RequestMapping("/signin/do")
-    public String signin(HttpServletRequest request, RedirectAttributes redirect){
+    @RequestMapping("/api/loginCheck")
+    public @ResponseBody Object signin(HttpServletRequest request){
         //处理参数
         String password=getMD5(request.getParameter("password"));
         String username=request.getParameter("username");
         //验证用户名密码
         int loginVerify=userService.login(username,password);
 
+        HashMap<String, String> res = new HashMap<String, String>();
+
+        //登录成功
         if(loginVerify == 2){
-            System.out.println("登录成功!");
             User user =userService.getUserByUsername(username);
             Integer userId=user.getId();
             //添加积分
@@ -132,16 +137,17 @@ public class UserController {
             log.setLoginTime(new Date());
             boolean ifSuccAddLog=loginLogService.addLog(log);
 
-            return "redirect:/";
-        }else if (loginVerify == 1){
-
-            System.out.println("密码错误!");
-            return "redirect:/signin";
-        }else {
-
-            System.out.println("用户名不存在!");
-            return "redirect:/signin";
+            res.put("stateCode", "2");
         }
+        //密码错误
+        else if (loginVerify == 1){
+            res.put("stateCode", "1");
+        }
+        //用户名不存在
+        else {
+            res.put("stateCode", "0");
+        }
+        return res;
     }
 
     /**
