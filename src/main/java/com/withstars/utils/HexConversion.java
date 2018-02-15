23601311fb32f8869d78f835f063f9ff9c862c14
withstars <15535822816@163.com>
2018-02-15 15:53:04 +0800

    Create utils

---
+++ src/main/java/com/withstars/utils/HexConversion.java
@@ -0,0 +1,33 @@
+package com.withstars.utils;
+
+import java.security.MessageDigest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: withstars
+ * Date: 2018-02-15
+ * Time: 10:57
+ * Mail: withstars@126.com
+ */
+public class HexConversion {
+
+    /**
+     * 二进制转十六进制
+     */
+    public static String bytesToHex(byte[] bytes) {
+        StringBuffer hexStr = new StringBuffer();
+        int num;
+        for (int i = 0; i < bytes.length; i++) {
+            num = bytes[i];
+            if(num < 0) {
+                num += 256;
+            }
+            if(num < 16){
+                hexStr.append("0");
+            }
+            hexStr.append(Integer.toHexString(num));
+        }
+        return hexStr.toString().toUpperCase();
+    }
+}
---
+++ src/main/java/com/withstars/utils/ProduceMD5.java
@@ -0,0 +1,30 @@
+package com.withstars.utils;
+
+import java.security.MessageDigest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: withstars
+ * Date: 2018-02-15
+ * Time: 10:56
+ * Mail: withstars@126.com
+ */
+public class ProduceMD5 {
+
+    /**
+     * 生成MD5值
+     */
+    public static String getMD5(String message) {
+        String md5 = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");  // 创建一个md5算法对象
+            byte[] messageByte = message.getBytes("UTF-8");
+            byte[] md5Byte = md.digest(messageByte);              // 获得MD5字节数组,16*8=128位
+            md5 = HexConversion.bytesToHex(md5Byte);                            // 转换为16进制字符串
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return md5;
+    }
+}
